4b71f05f2429690df0c3dc69cc45f246a2b952a0
rufunt <jazzoo@mail.ru>
2018-09-01 22:34:54 +0000

    Add Tasks Model and Controller and Update Projects controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180901213245) do
+ActiveRecord::Schema.define(version: 20180901214951) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -19,5 +19,14 @@ ActiveRecord::Schema.define(version: 20180901213245) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
 
 end
