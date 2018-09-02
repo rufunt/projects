b740e0205a43c88e26c141a1209d718c7cde73ee
rufunt <jazzoo@mail.ru>
2018-09-02 20:55:45 +0000

    Update app

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,15 @@
 class TasksController < ApplicationController
+  
+  def create
+    
+    @task = @project.task.create(params[:task].permit(:content))
+    
+    redirect_to @project
+  end
+  
+  private
+  
+  def set_project
+    @project = Project.find(params[project_id])
+  end
 end
